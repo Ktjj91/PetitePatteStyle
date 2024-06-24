e7fdd3fa13fb616c7d9cc3e2501d2250329a9bdd
Ktjj91 <kingsleythomas33@gmail.com>
2024-06-24 23:21:57 +0200

    product details

---
+++ app/product/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProductDetails from "@/components/ui/ProductDetails";
+
+export default function Page() {
+    return (
+        <div>
+            <ProductDetails />
+        </div>
+    )
+}
--- components/ui/Galerie.tsx
+++ components/ui/Galerie.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import ProductDetails from "@/components/ui/ProductDetails";
 
 export default function Galerie() {
     return (
         <section className="max-w-[1000px] w-full mx-auto p-5">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-
             </div>
 
         </section>
