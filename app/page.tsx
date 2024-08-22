f3a9e2da464a14a31668a17c3612a105f5cd969a
Ktjj91 <kingsleythomas33@gmail.com>
2024-08-23 01:06:16 +0200

    vitest home

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,25 @@ import {useProductStore} from "@/app/store";
 import Collection from "@/components/ui/Collection";
 
 export default function Home() {
-    const setProducts =  useProductStore.use.setProducts();
+    const setProducts = useProductStore.use.setProducts();
     const fetchData = async () => {
-         const response = await fetch("/api/products");
-             if (!response.ok) {
-                 throw new Error('Failed to fetch data')
-             }
-             const {products} = await response.json();
-             setProducts(products);
-     }
+        const response = await fetch(`${process.env.DNS || 'http://localhost:3000'}/api/products`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch data')
+        }
+        const {products} = await response.json();
+        setProducts(products);
+    }
     useEffect(() => {
         fetchData().then();
     }, []);
 
-  return (
-  <main className="flex-grow">
-   <Header />
-      <Collection />
-    <Galerie />
-  </main>
-  );
+
+    return (
+        <main className="flex-grow">
+            <Header/>
+            <Collection/>
+            <Galerie/>
+        </main>
+    );
 }
