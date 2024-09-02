34a40b9a22d7dc0bda941f568bd49b714c55f9fc
Ktjj91 <kingsleythomas33@gmail.com>
2024-09-02 13:42:46 +0200

    swa apifoler

--- lib/swagger.ts
+++ lib/swagger.ts
@@ -2,11 +2,11 @@ import { createSwaggerSpec } from "next-swagger-doc";
 
 export const getApiDocs = async () => {
     const spec = createSwaggerSpec({
-        apiFolder: "app/api", // define api folder under app folder
+        apiFolder: "/home/kingsley/PetitePatteStyle/app/api",
         definition: {
             openapi: "3.0.0",
             info: {
-                title: "Documentation api petitepattestyle",
+                title: "Documentation api Petitepattestyle",
                 version: "1.0",
             },
             components: {
